ac3bb83c29740a5eaf8f7e30b47b6f1b3d030a8b
Tom Lee <tom_t_lee@yahoo.com>
2015-04-20 20:47:56 -0400

    Create db video seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Video.create(title: "Southpark", description: "Colorado town of crazy kids",
+small_cover_url: "/public/tmp/south_park.jpg", large_cover_url: "/public/tmp/south_park.jpg")
+Video.create(title: "Futurama", description: "Fry discovers the future",
+small_cover_url: "/public/tmp/futurama.jpg", large_cover_url: "/public/tmp/futurama.jpg")
+Video.create(title: "Monk", description: "Crazy unorthadox detective gets results",
+small_cover_url: "/public/tmp/monk_large.jpg", large_cover_url: "/public/tmp/monk_small.jpg")
