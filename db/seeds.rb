c2eac7deff70bedcc0b380ef35644fe028f782f5
Tom Lee <tom_t_lee@yahoo.com>
2015-04-22 18:01:39 +0000

    Update categories in db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Video.create(title: "Southpark", description: "Colorado town of crazy kids", category_id: 1, 
-              small_cover_url: "south_park.jpg", large_cover_url: "south_park.jpg")
-Video.create(title: "Futurama", description: "Fry discovers the future", category_id: 2, 
-              small_cover_url: "futurama.jpg", large_cover_url: "futurama.jpg")
-Video.create(title: "Monk", description: "Crazy unorthadox detective gets results", category_id: 3, 
-              small_cover_url: "monk.jpg", large_cover_url: "monk_large.jpg")
-Video.create(title: "Family Guy", description: "Sick, twisted, and politically incorrect, the animated series features the adventures of the Griffin family", small_cover_url: "family_guy.jpg", large_cover_url: "family_guy.jpg", category_id: 1)
 Category.create(name: "Comedy")
 Category.create(name: "Action")
 Category.create(name: "Drama")
+Video.create(title: "Southpark", description: "Colorado town of crazy kids", category: "Comedy", 
+              small_cover_url: "south_park.jpg", large_cover_url: "south_park.jpg")
+Video.create(title: "Futurama", description: "Fry discovers the future", category: "Action", 
+              small_cover_url: "futurama.jpg", large_cover_url: "futurama.jpg")
+Video.create(title: "Monk", description: "Crazy unorthadox detective gets results", category: "Drama", 
+              small_cover_url: "monk.jpg", large_cover_url: "monk_large.jpg")
+Video.create(title: "Family Guy", description: "Sick, twisted, and politically incorrect, the animated series features the adventures of the Griffin family", small_cover_url: "family_guy.jpg", large_cover_url: "family_guy.jpg", category: "Comedy")
+
