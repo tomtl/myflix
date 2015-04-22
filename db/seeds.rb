f09146279c8622d659614c69f388390d3796fd40
Tom Lee <tom_t_lee@yahoo.com>
2015-04-22 12:51:22 +0000

    Add categories table and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Video.create(title: "Southpark", description: "Colorado town of crazy kids",
+Video.create(title: "Southpark", description: "Colorado town of crazy kids", category_id: 1, 
               small_cover_url: "south_park.jpg", large_cover_url: "south_park.jpg")
-Video.create(title: "Futurama", description: "Fry discovers the future",
+Video.create(title: "Futurama", description: "Fry discovers the future", category_id: 2, 
               small_cover_url: "futurama.jpg", large_cover_url: "futurama.jpg")
-Video.create(title: "Monk", description: "Crazy unorthadox detective gets results",
+Video.create(title: "Monk", description: "Crazy unorthadox detective gets results", category_id: 3, 
               small_cover_url: "monk.jpg", large_cover_url: "monk_large.jpg")
+Category.create(name: "Comedy")
+Category.create(name: "Action")
+Category.create(name: "Drama")
