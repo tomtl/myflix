265b5ed7dae739eadf7c02acddff0181b1431a0e
Tom Lee <tom_t_lee@yahoo.com>
2015-04-24 19:32:55 -0400

    Update video spec naming

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,10 +1,9 @@
 require 'spec_helper'
 
-describe Myflix do
+describe Video do
   it "saves itself" do
     video = Video.new(title: "Amy", description: "Video about Amy", category_id: 1)
     video.save
-    expect(Video.first.title).to eq("Amy")
-    expect(Video.first.description).to eq("Video about Amy")
+    expect(Video.last).to eq(video)
   end
 end
