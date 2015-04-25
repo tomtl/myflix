1c02a32fc332d0e73c86787f51dd298cd7dd8960
Tom Lee <tom_t_lee@yahoo.com>
2015-04-25 13:32:30 -0400

    Add shoulda matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 
 describe Category do
-  it "saves itself" do
-    category = Category.create(name: "Comedies")
-    expect(Category.first).to eq(category)
-  end
-
-  it "has many videos" do
-    comedies = Category.create(name: "Comedies")
-    southpark = Video.create(title: "Southpark", description: "Funny video!", category: comedies)
-    futurama = Video.create(title: "Futurama", description: "Space travelling", category: comedies)
-    expect(comedies.videos).to include(futurama, southpark)
-  end
+  it { should have_many(:videos)}
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,15 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "Amy", description: "Video about Amy", category_id: 1)
-    video.save
-    expect(Video.last).to eq(video)
-  end
-
-  it "belongs to category" do
-    dramas = Category.create(name: "dramas")
-    monk = Video.create(title: "Monk", description: "Detective show", category: dramas)
-    expect(monk.category).to eq(dramas)
-  end
+  it { should belong_to(:category)}
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:description)}
 end
