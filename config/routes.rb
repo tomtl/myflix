263239ccba43c4e69bdd08ab6b42850647166164
Tom Lee <tom_t_lee@yahoo.com>
2015-04-26 15:01:01 -0400

    Add title search to UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Myflix::Application.routes.draw do
   get '/home/', to: 'videos#index'
   get 'ui(/:action)', controller: 'ui'
 
-  resources :videos, only: [:index, :show]
+  resources :videos, only: [:index, :show] do
+      collection do
+        post :search, to: "videos#search"
+      end
+  end
+
   resources :categories, only: [:show]
 end
