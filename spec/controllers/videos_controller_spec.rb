7e2625db9610fddcf20a2a2030b04fc55dc97363
Tom Lee <tom_t_lee@yahoo.com>
2015-05-04 14:57:10 +0000

    Add Fabricator and Faker gems. Update videos#show spec

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,23 +2,17 @@ require 'spec_helper'
 
 describe VideosController do
   describe "GET show" do
-    context "with authenticated users" do
-      before do
-        john = User.create(email: "example@example.com", password: "password", full_name: "John John")
-        session[:user_id] = john.id
-      end
-
-      it "sets the @video variable" do
-        futurama = Video.create(title:"Futurama", description: "Space travel")
-        get :show, id: Video.first.id
-        expect(assigns(:video)).to eq(futurama)
-      end
-
-      it "renders the show template" do
-        futurama = Video.create(title:"Futurama", description: "Space travel")
-        get :show, id: Video.first.id
-        response.should render_template :show
-      end
+    it "sets the @video for authenticated user" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(assigns(:video)).to eq(video)
+    end
+    
+    it "redirects the user to the sign in page for unauthenticated user" do
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(response).to redirect_to sign_in_path
     end
   end
 
@@ -29,7 +23,7 @@ describe VideosController do
         session[:user_id] = john.id
       end
 
-      it "sets the @search variable" do
+      it "sets the @search" do
         futurama = Video.create(title:"Futurama", description: "Space travel")
         get :search, search_term:  Video.first.title
         expect(assigns(:results)).to eq([futurama])
