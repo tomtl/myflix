d8496e98cf25055dfd0678f44635fbe4dd40e5aa
Tom Lee <tom_t_lee@yahoo.com>
2015-05-05 16:02:43 +0000

    refactor sessions controller spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -16,64 +16,59 @@ describe SessionsController do
   
   describe "POST create" do
     context "with valid credentials" do
-      it "puts the signed in user in the session" do
-        alice = Fabricate(:user)
-        post :create, email: alice.email, password: alice.password
-        expect(session[:user_id]).to eq(alice.id)
+      before do
+        @alice = Fabricate(:user)
+        post :create, email: @alice.email, password: @alice.password     
       end
       
-      # Note: paused video at 15:30
+      it "puts the signed in user in the session" do
+        expect(session[:user_id]).to eq(@alice.id)
+      end
 
       it "redirects to the home path" do
-        alice = Fabricate(:user)
-        post :create, email: alice.email, password: alice.password
         expect(response).to redirect_to home_path
       end
       
       it "sets the notice" do
-        alice = Fabricate(:user)
-        post :create, email: alice.email, password: alice.password
         expect(flash[:notice]).not_to be_blank
       end
     end
     
     context "with invalid creadentials" do
-      it "does not put the signed in user in the session" do
+      before do
         alice = Fabricate(:user)
-        post :create, email: alice.email, password: "wrong-password"
+        post :create, email: alice.email, password: alice.password + "wrong"
+      end
+      
+      it "does not put the signed in user in the session" do
         expect(session[:user_id]).to be_nil
       end
       
       it "redirects to the sign in page" do
-        alice = Fabricate(:user)
-        post :create, email: alice.email, password: "wrong-password"
         expect(response).to redirect_to sign_in_path
       end
       
       it "sets the error message" do
-        alice = Fabricate(:user)
-        post :create, email: alice.email, password: "wrong-password"
         expect(flash[:error]).not_to be_blank
       end
     end
   end
   
   describe "GET destroy" do
-    it "clears the session for the user" do
+    before do
       session[:user_id] = Fabricate(:user).id
-      get :destroy
+      get :destroy     
+    end
+    
+    it "clears the session of the user" do
       expect(session[:user_id]).to be_nil
     end
     
     it "redirects to the root path" do
-      session[:user_id] = Fabricate(:user).id
-      get :destroy
       expect(response).to redirect_to root_path
     end
     
     it "sets the notice" do
-      session[:user_id] = Fabricate(:user).id
-      get :destroy
       expect(flash[:notice]).not_to be_blank
     end
   end
