a9e54a5932007f95c39e24f98a9dcac48f1ed162
Tom Lee <tom_t_lee@yahoo.com>
2015-05-05 15:35:44 +0000

    Create sessions controller spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -24,9 +24,57 @@ describe SessionsController do
       
       # Note: paused video at 15:30
 
-      it "redirects to the home path"
-      it "sets the notice"
+      it "redirects to the home path" do
+        alice = Fabricate(:user)
+        post :create, email: alice.email, password: alice.password
+        expect(response).to redirect_to home_path
+      end
+      
+      it "sets the notice" do
+        alice = Fabricate(:user)
+        post :create, email: alice.email, password: alice.password
+        expect(flash[:notice]).not_to be_blank
+      end
     end
-    context "with invalid creadentials"
+    
+    context "with invalid creadentials" do
+      it "does not put the signed in user in the session" do
+        alice = Fabricate(:user)
+        post :create, email: alice.email, password: "wrong-password"
+        expect(session[:user_id]).to be_nil
+      end
+      
+      it "redirects to the sign in page" do
+        alice = Fabricate(:user)
+        post :create, email: alice.email, password: "wrong-password"
+        expect(response).to redirect_to sign_in_path
+      end
+      
+      it "sets the error message" do
+        alice = Fabricate(:user)
+        post :create, email: alice.email, password: "wrong-password"
+        expect(flash[:error]).not_to be_blank
+      end
+    end
+  end
+  
+  describe "GET destroy" do
+    it "clears the session for the user" do
+      session[:user_id] = Fabricate(:user).id
+      get :destroy
+      expect(session[:user_id]).to be_nil
+    end
+    
+    it "redirects to the root path" do
+      session[:user_id] = Fabricate(:user).id
+      get :destroy
+      expect(response).to redirect_to root_path
+    end
+    
+    it "sets the notice" do
+      session[:user_id] = Fabricate(:user).id
+      get :destroy
+      expect(flash[:notice]).not_to be_blank
+    end
   end
 end
