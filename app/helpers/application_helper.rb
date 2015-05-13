9508a0408c715991543557fc643df25830e654c3
Tom Lee <tom_t_lee@yahoo.com>
2015-05-13 14:07:29 +0000

    Start adding rating update to my queue page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def stars_list
+  def options_for_video_reviews(selected=nil)
+    options_for_select([5,4,3,2,1].map { |number| [pluralize(number, "Star")] }, selected)
+  end
+  
+  def rating_values_list
     [5,4,3,2,1].map { |number| [pluralize(number, "Star")] }
   end
 end
