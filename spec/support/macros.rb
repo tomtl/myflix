d6daae0eb6ca0565eb0eb1becfb4df98d98cebe7
Tom Lee <tom_t_lee@yahoo.com>
2015-05-31 18:42:50 -0400

    Add user following feature spec

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -11,5 +11,9 @@ def sign_in(user = nil)
   visit sign_in_path
   fill_in "Email Address", with: user.email
   fill_in "Password", with: user.password
-  click_button "Sign in"  
+  click_button "Sign in"
+end
+
+def click_on_video_on_home_page(video)
+  find("a[href='/videos/#{video.id}']").click
 end
