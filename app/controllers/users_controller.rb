c6be96bf6bc1c8e9bfdcb5802e2d08e5cad8aa65
Tom <tom_t_lee@yahoo.com>
2015-06-01 19:02:29 +0000

    Start adding mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      AppMailer.send_welcome_email(@user).deliver
       flash[:notice] = "You have registered successfully"
       session[:user_id] = @user.id
       redirect_to home_path
