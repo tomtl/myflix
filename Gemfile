7b8a9e9aab1c3150e2146b15fef02d19a61a60ac
Tom <tom_t_lee@yahoo.com>
2015-06-05 16:08:02 +0000

    Add user resets password feature spec

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ group :test do
   gem 'shoulda-matchers'
   gem 'capybara'
   gem 'launchy'
+  gem 'capybara-email', github: 'dockyard/capybara-email'
 end
 
 group :development do
---
+++ spec/features/user_resets_password_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+feature 'User Resets Password' do
+  scenario' user successfully resets password' do
+    user = Fabricate(:user, password: "old_password")
+    visit sign_in_path
+    click_link "Forgot password?"
+    fill_in "Email address", with: user.email
+    click_button "Send Email"
+
+    open_email(user.email)
+    current_email.click_link "Reset my password"
+
+    fill_in "New password", with: "new_password"
+    click_button "Reset Password"
+
+    fill_in "Email Address", with: user.email
+    fill_in "Password", with: "new_password"
+    click_button "Sign in"
+    expect(page).to have_content("Welcome, #{user.full_name}")
+  end
+end
