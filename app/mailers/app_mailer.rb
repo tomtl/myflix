b540f3e8279a4b96543d2deee4fa6ce14a814179
Tom Lee <tom_t_lee@yahoo.com>
2015-06-08 20:50:13 -0400

    Add mailgun to production - cleanup app_mailer

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,7 +1,9 @@
 class AppMailer < ActionMailer::Base
   def send_welcome_email(user)
     @user = user
-    mail to: user.email, from: "info@myflix.com", subject: "Welcome to MyFlix!"
+    mail to: user.email,
+         from: "info@myflix.com",
+         subject: "Welcome to MyFlix!"
   end
 
   def send_forgot_password(user)
