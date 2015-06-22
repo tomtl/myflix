ff4df528bf5688587dceea8ff8e8905db42a9cdd
Tom <tom_t_lee@yahoo.com>
2015-06-22 18:27:48 +0000

    Continue working on S3 uploading

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,14 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.storage    = :aws
-    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+    config.aws_bucket = ENV['S3_BUCKET_NAME']
     config.aws_acl    = :'public-read'
     config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
   
     config.aws_credentials = {
-      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      # region:            ENV.fetch('AWS_REGION')
+      access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      # region:            ENV['AWS_REGION']
     }
   else
     config.storage = :file
