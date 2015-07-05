6b10b61de8396b77fa7809cc7eb34852a5cf7318
Tom Lee <tom_t_lee@yahoo.com>
2015-07-05 19:56:38 -0400

    Start refactor of users controller spec

--- spec/services/user_signup_spec.rb
+++ spec/services/user_signup_spec.rb
@@ -2,6 +2,8 @@ require "spec_helper"
 
 describe UserSignup do
   describe "#sign_up" do
-    context "with valid personal info and valid card"
+    context "with valid personal info and valid card" do
+      
+    end
   end
 end
